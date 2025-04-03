d5549b5311b74d00c8f06fee18bb5f502611e495
Arnaud <arnaud@status.im>
2025-04-03 13:18:14 +0200

    Fix settings test

--- e2e/settings.spec.ts
+++ e2e/settings.spec.ts
@@ -8,23 +8,35 @@ import test, { expect } from "@playwright/test";
 //     await expect(page.locator('span').filter({ hasText: 'success ! The log level has' }).locator('b')).toBeVisible();
 // })
 
-test('update the URL with wrong URL applies', async ({ page }) => {
-    await page.goto('/dashboard');
-    await page.locator('a').filter({ hasText: 'Settings' }).click();
-    await page.getByLabel('Address').click();
-    await page.getByLabel('Address').fill('hello');
-    await expect(page.getByLabel('Address')).toHaveAttribute("aria-invalid")
-    await expect(page.locator(".refresh svg")).toHaveAttribute("color", "#494949")
-    await page.getByLabel('Address').fill('http://127.0.0.1:8079');
-    await expect(page.getByLabel('Address')).not.toHaveAttribute("aria-invalid")
-    await expect(page.locator(".refresh svg")).not.toHaveAttribute("aria-disabled")
-    await expect(page.getByLabel('Address')).toHaveValue("http://127.0.0.1")
-    await expect(page.getByLabel('Port')).toHaveValue("8079")
-    await page.locator(".refresh").click()
-    await expect(page.locator(".health-checks ul li").nth(2).getByTestId("icon-error")).toBeVisible()
-    await expect(page.locator(".health-checks ul li").nth(2).getByTestId("icon-success")).not.toBeVisible()
-    await page.getByLabel('Address').fill('http://127.0.0.1:8080');
-    await page.locator(".refresh").click()
-    await expect(page.locator(".health-checks ul li").nth(2).getByTestId("icon-error")).not.toBeVisible()
-    await expect(page.locator(".health-checks ul li").nth(2).getByTestId("icon-success")).toBeVisible()
-})
+test("update the URL with wrong URL applies", async ({ page }) => {
+  await page.goto("/dashboard");
+  await page.locator("a").filter({ hasText: "Settings" }).click();
+  await page.getByLabel("Address").click();
+  await page.getByLabel("Address").fill("hello");
+  await expect(page.getByLabel("Address")).toHaveAttribute("aria-invalid");
+  await expect(page.locator(".refresh svg")).toHaveAttribute(
+    "color",
+    "#494949"
+  );
+  await page.getByLabel("Address").fill("http://127.0.0.1:8079");
+  await expect(page.getByLabel("Address")).not.toHaveAttribute("aria-invalid");
+  await expect(page.locator(".refresh svg")).not.toHaveAttribute(
+    "aria-disabled"
+  );
+  await expect(page.getByLabel("Address")).toHaveValue("http://127.0.0.1:8079");
+  await page.locator(".refresh").click();
+  await expect(
+    page.locator(".health-checks ul li").nth(2).getByTestId("icon-error")
+  ).toBeVisible();
+  await expect(
+    page.locator(".health-checks ul li").nth(2).getByTestId("icon-success")
+  ).not.toBeVisible();
+  await page.getByLabel("Address").fill("http://127.0.0.1:8080");
+  await page.locator(".refresh").click();
+  await expect(
+    page.locator(".health-checks ul li").nth(2).getByTestId("icon-error")
+  ).not.toBeVisible();
+  await expect(
+    page.locator(".health-checks ul li").nth(2).getByTestId("icon-success")
+  ).toBeVisible();
+});
